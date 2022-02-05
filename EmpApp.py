2569a79bfcdba8864bac162c2c9d9626ac1c6880
manojkumar619 <49521732+manojkumar619@users.noreply.github.com>
2022-02-05 18:48:20 +0530

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -159,4 +159,4 @@ def FetchData():
                            image_url=image_url)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
